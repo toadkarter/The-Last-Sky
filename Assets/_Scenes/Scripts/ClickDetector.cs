35bfff4a7d2de0fe0e630779886613323d5f983c
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2024-01-27 14:38:32 +0000

    Set up click detector behaviour.

---
+++ Assets/_Scenes/Scripts/ClickDetector.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Scenes.Scripts
+{
+    public class ClickDetector : MonoBehaviour
+    {
+        private Camera camera;
+
+        public delegate void ValidClickCallback(GameObject gameObject);
+
+        public event ValidClickCallback OnValidClick;
+
+        private void Start()
+        {
+            camera = GetComponent<Camera>();
+            if (camera == null)
+            {
+                Debug.Log("Could not find camera");
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit raycastHit;
+                Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out raycastHit, 100f))
+                {
+                    if (raycastHit.transform == null)
+                    {
+                        return;
+                    }
+                    
+                    IClickable clickableObject = raycastHit.transform.gameObject.GetComponent<IClickable>();
+
+                    if (clickableObject != null)
+                    {
+                        OnValidClick(raycastHit.transform.gameObject);
+                    }
+                }
+            }
+        }
+    }
+}
--- Assets/_Scenes/Scripts/GameLoop.cs
+++ Assets/_Scenes/Scripts/GameLoop.cs
@@ -6,9 +6,19 @@ namespace _Scenes.Scripts
     public class GameLoop : MonoBehaviour
     {
         [SerializeField] private Player playerClass = null;
-
+        [SerializeField] private ClickDetector clickDetector = null;
+         
         private Player player1 = null;
         private Player player2 = null;
 
+        private void Start()
+        {
+            clickDetector.OnValidClick += OnValidClick;
+        }
+
+        public void OnValidClick(GameObject gameObject)
+        {
+            Debug.Log(gameObject);
+        }
     }
 }
